5b0f664c612bdc3e92ea039bf95c13728d3b9c76
waqas ahmad <waqaskanju@gmail.com>
2023-12-21 02:18:27 +0500

    add PF subject

---
+++ data/pf_data.js
@@ -0,0 +1,39 @@
+// Programming Fundamentals
+const PF_MCQS = {
+  mcq1: [{
+     id: '1',
+  subject_name: 'PF',
+  chapter_name: 'Introduction',
+  question: 'Which of the following is NOT a type of algorithm?',
+  option_one: 'Program',
+  option_two: 'Flowchart',
+  option_three: 'Decision Table',
+  option_four: 'Pseudocode',
+  answer: 'A',
+  explanation: 'A program is not a type of algorithm. An algorithm is a step-by-step procedure or set of rules for solving a problem, while a program is a set of instructions written in a specific programming language to be executed by a computer. Although a program may implement an algorithm, it is not considered a type of algorithm itself. Flowchart, decision table, and pseudocode, on the other hand, are different ways to represent algorithms'
+  },],
+  mcq2: [{
+     id: '2',
+  subject_name: 'ITC',
+  chapter_name: 'Introduction',
+  question: 'The symbol that represents comments in a flowchart is',
+  option_one: 'A procedure symbol',
+  option_two: 'An annotation symbol',
+  option_three: 'A flow line',
+  option_four: 'A subroutine',
+  answer: 'B',
+  explanation: 'Annotation symbol is used in flowcharts to provide additional information or explanations about the flowchart or specific parts of it. It is typically represented by a rectangle with a dashed or dotted line, and it contains text that describes the purpose or functionality of the associated flowchart element'
+  },],
+  mcq3: [{
+     id: '3',
+  subject_name: 'ITC',
+  chapter_name: 'Introduction',
+  question: 'The part of a decision table in which all the conditions, based on which a decision is made, is written is:',
+  option_one: 'Condition Applied',
+  option_two: 'Condition Stub',
+  option_three: 'Action Taken',
+  option_four: 'Action Stub',
+  answer: 'B',
+  explanation: 'The condition stub is the part of a decision table where all the conditions, based on which a decision is made, are written. It represents the set of conditions that need to be evaluated in order to determine the appropriate action to take. The condition stub acts as a placeholder for the conditions and helps organize the decision table by clearly defining the conditions that need to be considered'
+  },],
+}
--- subjects.js
+++ subjects.js
@@ -1,183 +1,189 @@
 // This file contain subject and chapter names.
 const subjects = {
-    // Operating Systems
-    OS : {
-    chapters:[
-        {
-        id : '1',
-        name :'Introduction',
-        total: '50'
-        },
-        {
-        id : '2',
-        name :'Process',
-        total: '58'
-        },
-        {
-            id : '3',
-            name :'Distributed Communication',
-            total: '20'
-        },
-        {
-            id : '4',
-            name :'CPU Scheduling',
-            total: '44'
-        },
-        {
-            id : '5',
-            name :'Process Synchronization',
-            total: '62'
-        },
-        {
-            id : '6',
-            name :'Deadlock',
-            total: '54'
-        },
-        {
-            id : '7',
-            name :'Memory Management',
-            total: '84'
-        },
-        {
-            id : '8',
-            name :'I/O System',
-            total: '34'
-        },
-        {
-            id : '9',
-            name :'Real Time Operating Systems',
-            total: '36'
-        },
-        {
-            id : '10',
-            name :'Multimedia Systems',
-            total: '62'
-        },
-        {
-            id : '11',
-            name :'Security',
-            total: '62'
-        },
-        {
-            id : '12',
-            name :'Secondary Storage',
-            total: '9'
-        },
-        {
-            id : '13',
-            name :'Linux OS',
-            total: '42'
-        },
-        {
-            id : '14',
-            name :'Empty',
-            total: 'x'
-        },
-    ]
-
-    },
-    // Computer Networks.
-    CN : {
-        chapters:[
-            {
-            id : '1',
-            name :'Introduction',
-            total: '50'
-            },
-            {
-            id : '2',
-            name :'OSI Model',
-            total: '10'
-            },
-            {
-            id : '3',
-            name :'Security and Physical Layer',
-            total: '0'
-            },
-            {
-                id : '4',
-                name :'Mcqs in CS',
-                total: '10'
-                }
-        ]
-
-    },
-    // Data Structure and algorithums
-    DSA : {
-        chapters:[
-            {
-            id : '1',
-            name :'Introduction',
-            total: '0'
-            },
-            {
-            id : '2',
-            name :'Graph',
-            total: '15'
-            },
-        ]
-
-    },
-
-    // C++
-
-    CPP : {
-       chapters: [
-        {
-        id: '1',
-        name: 'Introduction',
-        total: '0'
-
-    }
-       ]
-    },
-    // C Programming.
-    CP : {
-        chapters: [
-         {
-         id: '1',
-         name: 'Mcqs in CS',
-         total: '10'
-
-     }
-        ]
-     },
-     SE : {
-        chapters: [
-         {
-         id: '1',
-         name: 'Mcqs in CS',
-         total: '61'
-
-     }
-        ]
-     },
-     DBS : {
-      chapters: [
-        {
-          id: '1',
-          name: 'Introduction',
-          total: '1'
-        }
-      ]
-     },
-     AI : {
-      chapters: [
-        {
-          id: '1',
-          name: 'Introduction',
-          total: '1'
-        }
-      ]
-     },
-      // Introduction to Computers.
-    ITC : {
-      chapters:[
-          {
-          id : '1',
-          name :'Introduction',
-          total: '130'
-          },
-      ],
+  // Operating Systems
+  OS: {
+    chapters: [
+      {
+        id: "1",
+        name: "Introduction",
+        total: "50",
       },
+      {
+        id: "2",
+        name: "Process",
+        total: "58",
+      },
+      {
+        id: "3",
+        name: "Distributed Communication",
+        total: "20",
+      },
+      {
+        id: "4",
+        name: "CPU Scheduling",
+        total: "44",
+      },
+      {
+        id: "5",
+        name: "Process Synchronization",
+        total: "62",
+      },
+      {
+        id: "6",
+        name: "Deadlock",
+        total: "54",
+      },
+      {
+        id: "7",
+        name: "Memory Management",
+        total: "84",
+      },
+      {
+        id: "8",
+        name: "I/O System",
+        total: "34",
+      },
+      {
+        id: "9",
+        name: "Real Time Operating Systems",
+        total: "36",
+      },
+      {
+        id: "10",
+        name: "Multimedia Systems",
+        total: "62",
+      },
+      {
+        id: "11",
+        name: "Security",
+        total: "62",
+      },
+      {
+        id: "12",
+        name: "Secondary Storage",
+        total: "9",
+      },
+      {
+        id: "13",
+        name: "Linux OS",
+        total: "42",
+      },
+      {
+        id: "14",
+        name: "Virtual Memory",
+        total: "x",
+      },
+    ],
+  },
+  // Computer Networks.
+  CN: {
+    chapters: [
+      {
+        id: "1",
+        name: "Introduction",
+        total: "50",
+      },
+      {
+        id: "2",
+        name: "OSI Model",
+        total: "10",
+      },
+      {
+        id: "3",
+        name: "Security and Physical Layer",
+        total: "0",
+      },
+      {
+        id: "4",
+        name: "Mcqs in CS",
+        total: "10",
+      },
+    ],
+  },
+  // Data Structure and algorithums
+  DSA: {
+    chapters: [
+      {
+        id: "1",
+        name: "Introduction",
+        total: "0",
+      },
+      {
+        id: "2",
+        name: "Graph",
+        total: "15",
+      },
+    ],
+  },
+
+  // C++
+
+  CPP: {
+    chapters: [
+      {
+        id: "1",
+        name: "Introduction",
+        total: "0",
+      },
+    ],
+  },
+  // C Programming.
+  CP: {
+    chapters: [
+      {
+        id: "1",
+        name: "Mcqs in CS",
+        total: "10",
+      },
+    ],
+  },
+  //Software Engineering
+  SE: {
+    chapters: [
+      {
+        id: "1",
+        name: "Mcqs in CS",
+        total: "61",
+      },
+    ],
+  },
+  // Database System
+  DBS: {
+    chapters: [
+      {
+        id: "1",
+        name: "Introduction",
+        total: "1",
+      },
+    ],
+  },
+  // Artificial Intelligence
+  AI: {
+    chapters: [
+      {
+        id: "1",
+        name: "Introduction",
+        total: "1",
+      },
+    ],
+  },
+  // Introduction to Computers.
+  ITC: {
+    chapters: [
+      {
+        id: "1",
+        name: "Introduction",
+        total: "130",
+      },
+    ],
+  },
+  PF: {
+    chapters: [
+      {
+        id: "1",
+        name: "Introduction",
+        total: "50",
+      },
+    ],
+  },
 };
